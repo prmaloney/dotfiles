1fc20a999eba4612a67c3a414e17c2dee7e1d556
Patrick Maloney <pmaloney16@MacBook-Pro-8.local>
2024-10-08 16:32:03 +0200

    add install script and gitignore

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+realpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+safe_link(){
+    local src="$1"
+    local dest="$HOME/`basename $src`"
+    [ ! -e "$dest" ] && ln -sf "$src" "$dest"
+}
+
+# Get root
+SCRIPT_PATH=`realpath $0`
+DOTFILES=`dirname $SCRIPT_PATH`
+
+# Install bin scripts
+safe_link "$DOTFILES/bin"
+# Install other dotfiles
+for f in `ls -A $DOTFILES`; do
+    safe_link "$DOTFILES/$f"
+done
